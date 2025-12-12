5156cfc0bd885edbb1a46d754d824bdc29686d60
asushanthreddy9-624 <asushanthreddy9@gmail.com>
2025-12-12 23:18:05 +0800

    Create abstract Person class with fields and methods

---
+++ Person.java
@@ -0,0 +1,21 @@
+public abstract class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Abstract method (must be overridden by Student)
+    public abstract void displayInfo();
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
